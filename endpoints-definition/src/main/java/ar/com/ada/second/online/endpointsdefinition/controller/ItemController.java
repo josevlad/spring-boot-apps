79ae0c1b2287c8d8df14f52868b12b1ac37c832e
Vladimir Castañeda <vladimir29castaneda@gmail.com>
2021-02-12 22:12:05 -0300

    delete item by id endpoint

--- endpoints-definition/src/main/java/ar/com/ada/second/online/endpointsdefinition/controller/ItemController.java
+++ endpoints-definition/src/main/java/ar/com/ada/second/online/endpointsdefinition/controller/ItemController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/items")
@@ -89,5 +86,17 @@ public class ItemController {
         return ResponseEntity.status(httpStatus).body(itemById);
     }
 
+    @DeleteMapping({ "/{itemId}", "/{itemId}/" })
+    public ResponseEntity deleteItemByIdMethod(@PathVariable Long itemId) {
 
+        Boolean hasDelete = items.removeIf(item -> item.getId().equals(itemId));
+
+        Map<String, String> itemNotFound = new HashMap<>();
+        itemNotFound.put("error", HttpStatus.BAD_REQUEST.toString());
+        itemNotFound.put("message", "item id not exist");
+
+        return (hasDelete)
+                ? ResponseEntity.noContent().build() // 204
+                : ResponseEntity.badRequest().body(itemNotFound); // 400
+    }
 }
